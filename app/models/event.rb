ae3d7a4f7b1e37eb2e1719992e93a0fd9217880d
konstantin0s <constantintofan85@gmail.com>
2017-12-13 13:27:43 +0100

    One-to-Many: Models

---
+++ app/models/event.rb
@@ -0,0 +1,17 @@
+class Event < ApplicationRecord
+
+
+
+
+  belongs_to :user
+  #has_and_belongs_to_many :categories
+  has_many :events, dependent: :destroy
+
+  validates :name, presence: true
+  validates :starts_at, presence: true
+  validates :ends_at, presence: true
+  #validates_date :starts_at, on_or_before: lambda { ends_at }
+  validates :description, presence: true, length: { maximum: 500 }
+
+  validate :start_must_be_before_end_time
+end
