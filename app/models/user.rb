3cd7a48190aacb128c95da8347135f4f8a504779
konstantin0s <constantintofan85@gmail.com>
2017-12-13 17:20:58 +0100

    One-to-One Association

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,16 @@ class User < ApplicationRecord
 
          has_many :events
 
+         has_one :profile
+
+ def full_name
+   return profile.full_name if profile?
+
+   email
+ end
+
+ def profile?
+   profile.present? && profile.persisted?
+ end
+
        end
