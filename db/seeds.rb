03378b303dee637ebee4f1ad87f1514c3f5f3296
konstantin0s <constantintofan85@gmail.com>
2017-12-14 14:02:09 +0100

    last update on seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,24 +16,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-#require 'faker'
-
-#user = ["Name"]
-
-#5.times.do
-#  name = Faker::Name.name
-
-  # Add user to user_list
-#  user << [ name ]
-#end
-
-#user.each do |name|
-#  User.create(name: name)
-#end
 
 
+Profile.destroy_all
+Event.destroy_all
+User.destroy_all
 Category.destroy_all
- User.destroy_all
+
 
 dino = User.create!(
   email: 'dino@codaisseur.com',
